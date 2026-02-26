a56377c50d210c4b4bc6fd777173c8dfcce36ef6
Karnemahesh <karnemahesh19@gmail.com>
2026-02-26 14:39:06 +0530

    Fix typo in project title from 'Co people' to 'Copeople'

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const ProjectCard = ({ image, title, description, git, technologies }) => {
             {title=='Snap Shot' && <a href="#">
                 <img className="w-full rounded-t-lg h-auto object-cover " src={vpn} alt="" />
             </a>}
-            {title=='Co people' && <a href="#">
+            {title=='Copeople' && <a href="#">
                 <img className="w-full rounded-t-lg h-auto object-cover " src={Copeople} alt="" />
             </a>}
             <div className="p-4 sm:p-6">
